493c82769070b51e4dac65c0981eafd68ee3b50b
Daniel Kiss <littlesnorrboy@gmail.com>
2024-10-04 22:55:35 +0200

    init

---
+++ flake.nix
@@ -0,0 +1,58 @@
+{
+  description = "Bevy devshell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = inputs@{ self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        bevy-deps = with pkgs; [
+          udev
+          alsa-lib
+          xorg.libX11
+          xorg.libXcursor
+          xorg.libXi
+          xorg.libXrandr # To use the x11 feature
+          libxkbcommon
+          libGL
+          wayland # To use the wayland feature
+          libz
+          openssl
+          vulkan-loader
+          vulkan-extension-layer
+          vulkan-validation-layers # don't need them *strictly* but immensely helpful
+        ];
+        tools = with pkgs; [
+          pkg-config
+          cargo-edit
+          just
+          parallel
+        ];
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          buildInputs = [
+            # rust deps
+            mold
+            llvmPackages_latest.clang
+            stdenv
+            (rust-bin.nightly.latest.default.override {
+              extensions = [ "rust-src" "rust-analyzer" "rustfmt" ];
+              targets = [ ];
+            })
+          ] ++ bevy-deps ++ tools;
+          LD_LIBRARY_PATH = lib.makeLibraryPath (bevy-deps);
+        };
+      }
+    );
+}
+
---
+++ src/main.rs
@@ -0,0 +1,78 @@
+use std::f32::consts::{PI, TAU};
+
+use bevy::{color::palettes::css::*, prelude::*};
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .init_gizmo_group::<MyRoundGizmos>()
+        .insert_resource(TestParams::default())
+        .add_systems(Startup, setup)
+        .add_systems(Update, (draw_example_collection, rotate_phi))
+        .run();
+}
+
+// We can create our own gizmo config group!
+#[derive(Reflect, GizmoConfigGroup, Default, Clone, Copy)]
+struct MyRoundGizmos {}
+
+impl Default for TestParams {
+    fn default() -> Self {
+        Self {
+            phi: 1.0 * PI,
+            theta: PI / 2.0,
+            radius: 2.0,
+        }
+    }
+}
+
+#[derive(Debug, Resource)]
+struct TestParams {
+    pub phi: f32,
+    pub theta: f32,
+    pub radius: f32,
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(0., 1.5, 6.).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
+}
+
+fn rotate_camera(mut query: Query<&mut Transform, With<Camera>>, time: Res<Time>) {
+    let mut transform = query.single_mut();
+
+    transform.rotate_around(Vec3::ZERO, Quat::from_rotation_y(time.delta_seconds() / 2.));
+}
+
+fn draw_example_collection(
+    mut gizmos: Gizmos,
+    mut my_gizmos: Gizmos<MyRoundGizmos>,
+    params: Res<TestParams>,
+) {
+    let TestParams { phi, theta, radius } = *params;
+    my_gizmos.sphere(Vec3::ZERO, Quat::IDENTITY, radius, RED);
+
+    let (sphi, cphi) = phi.sin_cos();
+    let (stheta, ctheta) = theta.sin_cos();
+
+    let pos = Vec3::new(
+        radius * stheta * cphi,
+        radius * stheta * sphi,
+        radius * ctheta,
+    );
+
+    let right = -pos.cross(Vec3::Y);
+    let up = pos.cross(right).normalize_or_zero();
+
+    gizmos.line(pos, Vec3::ZERO, YELLOW);
+    gizmos.arrow(pos, pos + up, BLUE);
+
+    println!("pog? {:.3}", pos.dot(up));
+}
+
+fn rotate_phi(mut p: ResMut<TestParams>, time: Res<Time>) {
+    p.theta = (p.theta + PI / 4.0 * time.delta_seconds()) ;
+    p.phi = (p.phi + PI / 8.0 * time.delta_seconds()) ;
+}
