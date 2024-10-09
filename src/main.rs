71ccf779dd4d2ce69626039a0f4162e35485749a
Daniel Kiss <littlesnorrboy@gmail.com>
2024-10-09 10:16:30 +0200

    Write parameters on the UI

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,18 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .init_gizmo_group::<MyRoundGizmos>()
         .insert_resource(TestParams::default())
+        .insert_resource(Cartesian::default())
         .add_systems(Startup, setup)
-        .add_systems(Update, (draw_example_collection, rotate_phi, rotate_camera))
+        .add_systems(
+            Update,
+            (
+                draw_example_collection,
+                rotate_phi,
+                rotate_camera,
+                update_text,
+                update_cartesian_vectors,
+            ),
+        )
         .run();
 }
 
@@ -33,11 +43,81 @@ struct TestParams {
     pub radius: f32,
 }
 
+#[derive(Debug, Resource, Default)]
+struct Cartesian {
+    pub pos: Vec3,
+    pub up: Vec3,
+}
+
+#[derive(Component)]
+struct VectorText;
+
 fn setup(mut commands: Commands) {
     commands.spawn(Camera3dBundle {
         transform: Transform::from_xyz(0., 1.5, 6.).looking_at(Vec3::ZERO, Vec3::Y),
         ..default()
     });
+
+    let style = TextStyle {
+        font_size: 32.0,
+        ..Default::default()
+    };
+    commands
+        .spawn(
+            // Here we are able to call the `From` method instead of creating a new `TextSection`.
+            // This will use the default font (a minimal subset of FiraMono) and apply the default styling.
+            TextBundle::from_sections([
+                TextSection::new("Polar Pos: ", style.clone()),
+                TextSection::new("-----", style.clone()),
+                TextSection::new("\nCart. Pos: ", style.clone()),
+                TextSection::new("-----", style.clone()),
+                TextSection::new("\nUp: ", style.clone()),
+                TextSection::new("-----", style.clone()),
+                TextSection::new("\nUp dot Pos: ", style.clone()),
+                TextSection::new("-----", style.clone()),
+            ])
+            .with_style(Style {
+                position_type: PositionType::Absolute,
+                top: Val::Px(5.0),
+                left: Val::Px(15.0),
+                ..default()
+            }),
+        )
+        .insert(VectorText);
+}
+
+fn update_text(
+    mut query: Query<&mut Text, With<VectorText>>,
+    params: Res<TestParams>,
+    vec: Res<Cartesian>,
+) {
+    for mut t in query.iter_mut() {
+        t.sections[1].value = format!(
+            "theta={:.2} phi={:.2} r={:.2}",
+            params.theta, params.phi, params.radius
+        );
+        t.sections[3].value = format!("{:.2}", vec.pos);
+        t.sections[5].value = format!("{:.2}", vec.up);
+        t.sections[7].value = format!("{:.2}", vec.up.dot(vec.pos).abs());
+    }
+}
+
+fn update_cartesian_vectors(mut cart: ResMut<Cartesian>, params: Res<TestParams>) {
+    let TestParams { phi, theta, radius } = *params;
+    let (sphi, cphi) = phi.sin_cos();
+    let (stheta, ctheta) = theta.sin_cos();
+    cart.pos = Vec3::new(
+        radius * stheta * cphi,
+        radius * stheta * sphi,
+        radius * ctheta,
+    );
+
+    cart.up = Vec3::new(
+        -sphi * stheta.powi(2) * cphi,
+        stheta.powi(2) * cphi.powi(2) + ctheta.powi(2),
+        -sphi * stheta * ctheta,
+    )
+    .normalize();
 }
 
 fn rotate_camera(mut query: Query<&mut Transform, With<Camera>>, time: Res<Time>) {
@@ -49,28 +129,14 @@ fn rotate_camera(mut query: Query<&mut Transform, With<Camera>>, time: Res<Time>
 fn draw_example_collection(
     mut gizmos: Gizmos,
     mut my_gizmos: Gizmos<MyRoundGizmos>,
+    cart: Res<Cartesian>,
     params: Res<TestParams>,
 ) {
-    let TestParams { phi, theta, radius } = *params;
     my_gizmos
-        .sphere(Vec3::ZERO, Quat::IDENTITY, radius, RED)
+        .sphere(Vec3::ZERO, Quat::IDENTITY, params.radius, RED)
         .resolution(64);
 
-    let (sphi, cphi) = phi.sin_cos();
-    let (stheta, ctheta) = theta.sin_cos();
-
-    let pos = Vec3::new(
-        radius * stheta * cphi,
-        radius * stheta * sphi,
-        radius * ctheta,
-    );
-
-    let up = Vec3::new(
-        -sphi * stheta.powi(2) * cphi,
-        stheta.powi(2) * cphi.powi(2) + ctheta.powi(2),
-        -sphi * stheta * ctheta,
-    )
-    .normalize();
+    let Cartesian { pos, up } = *cart;
 
     gizmos.line(pos, Vec3::ZERO, YELLOW);
     gizmos.arrow(pos, pos + up, BLUE);
